f45f6a75973be96b336a0e39dc2e17a72743ec8c
st011310 <guseinovali27032003@gmail.com>
2024-08-30 15:11:55 +0300

    Sync

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const searchForm = document.querySelector('.search-form');
+
+    searchForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        alert('Поиск ещё не реализован.');
+    });
+});
--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const searchForm = document.querySelector('.search-form');
-
-    searchForm.addEventListener('submit', function (event) {
-        event.preventDefault();
-        alert('Поиск ещё не реализован.');
-    });
-});
